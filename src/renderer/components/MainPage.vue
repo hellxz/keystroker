e68fdd613e426f874a326febb0ff366422507f21
hellxz <hellxz001@foxmail.com>
2020-03-06 02:40:16 +0800

    识别输入并判定输入与对照区是否相同逻辑

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -38,8 +38,8 @@
             <div slot="top" class="split-pane" >
                 <div id="control-area"  readonly>
                   <!--当还没开始打字的时候，展示载文-->
-                  <span v-if="isStroking == false ">{{ article }}</span>
-                  <div v-html="textSpans"></div>
+                  <!-- <div v-html="textSpans"></div> -->
+                  <div id="article-block" v-html="article"></div>
                   <!-- <span class="type-err">在</span><span class="type-suc">工</span><span class="type-suc">作</span><span class="type-err">或</span><span class="type-n">学</span> -->
                   <!--
                     在工作或学习中遇到不开心的时候，不妨静下来好好想想，自己到底是对是错。生活中不是你对别人好，别人就该对你好，你要明白这个道理，每个人都有自己的原则，有人功利，有人善良，你不可能要求别人什么。有时间的话，不妨到处走走，在雄伟的高山之间，放声大喊，一吐心中的阴郁，在浪漫的大海之间，看潮起潮落，感悟人生的起伏跌宕，在落日余晖中感受天地的宁静，洗涤心中的贪念。当你遭遇失恋的时候，不要太悲伤，失去的并非能忘记，拥有的并非不会失去。那些缘来缘去都只是人生的一个瞬间，而人生里那些大段的时间，你会和那个懂你，爱你，珍惜你的人一起度过，好好珍惜在你身边默默陪着你的人。每个人都懂得这个道理，可是还是有人执迷不悟，苦苦执着于那个无缘的人，不肯放手，伤了自己，苦了别人。爱他或她就让那份美好的记忆藏在心底，我们都是在爱的挫折中，逐渐成长起来，才知道什么是爱，又该怎样去爱。希望多年以后再次相遇，还能洒脱的说一句，谢谢你曾经爱过我。当你的婚姻出现问题的时候，多些宽容，多些理解，少些责骂。婚姻本来就是两个人的事，一味的埋怨对方，根本不能解决任何问题。多想想曾经的美好回忆，想想两人同吃一碗面的艰难日子，想想曾经心里的感动。试着做一下换位思考，自己错在哪里，多给自己，也给对方一些机会，人都会有缺点，错误，多些包容，你会活得更轻松。如果真的缘尽了，不妨轻轻的放开手，让彼此都留下美好的回忆，道一声珍重再见，如果不能做朋友，也不可能做敌人，就做那个熟悉的陌生人吧。毕竟生活还是要继续，我们还要继续寻找那个有缘之人。当你和孩子出现问题的时候，不妨想想自己的青春年少，想想那些无知的轻狂岁月，我们都是从年轻走过来的。试着去了解今天的孩子想的什么，喜欢什么，当你以一个朋友的身份去了解他们的时候，你就已经是他们的朋友了，多些沟通，多些理解，多些包容，他们才会向你倾诉他们的烦恼，困惑，迷茫。人生不是十全十美，当你的心觉得累了的时候，不妨为自己的心找一个出口。换一个工作，换一种生活，也换一种对待生活的态度。
@@ -47,7 +47,7 @@
                 </div>
             </div>
             <div slot="bottom" class="split-pane">
-                <textarea id="follow-stroke-area" @keyup="startStroke">你工作在</textarea>
+                <textarea id="follow-stroke-area" @keyup="stroking" v-model="inputText"></textarea>
             </div>
         </Split>
     </div>
@@ -86,7 +86,7 @@
 </template>
 
 <script>
-
+import $ from 'jquery'
 export default {
   name: 'main-page',
   created () {
@@ -105,10 +105,18 @@ export default {
       isStroking: true,
       currentSegment: 0, // 当前输入到第几段
       strokedSum: 0, // 已打字数
-      defaultSplitSegmentSum: 3, // 默认文章字数分段数量
-      article: '在工作或学习中遇到不开心的时候，不妨静下来好好想想，自己到底是对是错。生活中不是你对别人好，别人就该对你好，你要明白这个道理，每个人都有自己的原则，有人功利，有人善良，你不可能要求别人什么。有时间的话，不妨到处走走，在雄伟的高山之间，放声大喊，一吐心中的阴郁，在浪漫的大海之间，看潮起潮落，感悟人生的起伏跌宕，在落日余晖中感受天地的宁静，洗涤心中的贪念。当你遭遇失恋的时候，不要太悲伤，失去的并非能忘记，拥有的并非不会失去。那些缘来缘去都只是人生的一个瞬间，而人生里那些大段的时间，你会和那个懂你，爱你，珍惜你的人一起度过，好好珍惜在你身边默默陪着你的人。每个人都懂得这个道理，可是还是有人执迷不悟，苦苦执着于那个无缘的人，不肯放手，伤了自己，苦了别人。爱他或她就让那份美好的记忆藏在心底，我们都是在爱的挫折中，逐渐成长起来，才知道什么是爱，又该怎样去爱。希望多年以后再次相遇，还能洒脱的说一句，谢谢你曾经爱过我。当你的婚姻出现问题的时候，多些宽容，多些理解，少些责骂。婚姻本来就是两个人的事，一味的埋怨对方，根本不能解决任何问题。多想想曾经的美好回忆，想想两人同吃一碗面的艰难日子，想想曾经心里的感动。试着做一下换位思考，自己错在哪里，多给自己，也给对方一些机会，人都会有缺点，错误，多些包容，你会活得更轻松。如果真的缘尽了，不妨轻轻的放开手，让彼此都留下美好的回忆，道一声珍重再见，如果不能做朋友，也不可能做敌人，就做那个熟悉的陌生人吧。毕竟生活还是要继续，我们还要继续寻找那个有缘之人。当你和孩子出现问题的时候，不妨想想自己的青春年少，想想那些无知的轻狂岁月，我们都是从年轻走过来的。试着去了解今天的孩子想的什么，喜欢什么，当你以一个朋友的身份去了解他们的时候，你就已经是他们的朋友了，多些沟通，多些理解，多些包容，他们才会向你倾诉他们的烦恼，困惑，迷茫。人生不是十全十美，当你的心觉得累了的时候，不妨为自己的心找一个出口。换一个工作，换一种生活，也换一种对待生活的态度。',
+      defaultSplitSegmentSum: 30, // 默认文章字数分段数量
+      // article: '在工作或学习中遇到不开心的时候，不妨静下来好好想想，自己到底是对是错。生活中不是你对别人好，别人就该对你好，你要明白这个道理，每个人都有自己的原则，有人功利，有人善良，你不可能要求别人什么。有时间的话，不妨到处走走，在雄伟的高山之间，放声大喊，一吐心中的阴郁，在浪漫的大海之间，看潮起潮落，感悟人生的起伏跌宕，在落日余晖中感受天地的宁静，洗涤心中的贪念。当你遭遇失恋的时候，不要太悲伤，失去的并非能忘记，拥有的并非不会失去。那些缘来缘去都只是人生的一个瞬间，而人生里那些大段的时间，你会和那个懂你，爱你，珍惜你的人一起度过，好好珍惜在你身边默默陪着你的人。每个人都懂得这个道理，可是还是有人执迷不悟，苦苦执着于那个无缘的人，不肯放手，伤了自己，苦了别人。爱他或她就让那份美好的记忆藏在心底，我们都是在爱的挫折中，逐渐成长起来，才知道什么是爱，又该怎样去爱。希望多年以后再次相遇，还能洒脱的说一句，谢谢你曾经爱过我。当你的婚姻出现问题的时候，多些宽容，多些理解，少些责骂。婚姻本来就是两个人的事，一味的埋怨对方，根本不能解决任何问题。多想想曾经的美好回忆，想想两人同吃一碗面的艰难日子，想想曾经心里的感动。试着做一下换位思考，自己错在哪里，多给自己，也给对方一些机会，人都会有缺点，错误，多些包容，你会活得更轻松。如果真的缘尽了，不妨轻轻的放开手，让彼此都留下美好的回忆，道一声珍重再见，如果不能做朋友，也不可能做敌人，就做那个熟悉的陌生人吧。毕竟生活还是要继续，我们还要继续寻找那个有缘之人。当你和孩子出现问题的时候，不妨想想自己的青春年少，想想那些无知的轻狂岁月，我们都是从年轻走过来的。试着去了解今天的孩子想的什么，喜欢什么，当你以一个朋友的身份去了解他们的时候，你就已经是他们的朋友了，多些沟通，多些理解，多些包容，他们才会向你倾诉他们的烦恼，困惑，迷茫。人生不是十全十美，当你的心觉得累了的时候，不妨为自己的心找一个出口。换一个工作，换一种生活，也换一种对待生活的态度。',
+      article: '',
       articleArr: [], // 文章分词后的数组，用于查询使用
-      textSpans: ''
+      textSpans: '',
+      keyInputSum: 0,
+      inputText: ''
+    }
+  },
+  watch: {
+    inputText: function () {
+      this.checkTrue()
     }
   },
   methods: {
@@ -120,12 +128,30 @@ export default {
     },
     loadArticle () {
       console.log('从剪贴板载文')
+      $('#article-block').html()
+      this.article = ''
       const { clipboard } = require('electron')
       console.log(clipboard.readText('selection'))
       this.article = clipboard.readText('selection')
+      this.articleArr = this.article.split('') // 整篇文章转成数组
+      let spansContact = ''
+      let currentTypeArr = this.articleArr.splice(0, this.defaultSplitSegmentSum)
+      for (var i in currentTypeArr) {
+        spansContact += '<span class="type-n">' + currentTypeArr[i] + '</span>'
+      }
+      console.log(spansContact)
+      this.textSpans = spansContact
+      this.strokeAreaText = '' // TODO 清空操作暂未生效
     },
     sendArticle () {
-      alert('发文')
+      $('#article-block').html()
+      this.article = ''
+      // 以下是临时测试用，正式时使用数据库或文件内数据
+      let xindechukou = '在工作或学习中遇到不开心的时候，不妨静下来好好想想，自己到底是对是错。生活中不是你对别人好，别人就该对你好，你要明白这个道理，每个人都有自己的原则，有人功利，有人善良，你不可能要求别人什么。有时间的话，不妨到处走走，在雄伟的高山之间，放声大喊，一吐心中的阴郁，在浪漫的大海之间，看潮起潮落，感悟人生的起伏跌宕，在落日余晖中感受天地的宁静，洗涤心中的贪念。当你遭遇失恋的时候，不要太悲伤，失去的并非能忘记，拥有的并非不会失去。那些缘来缘去都只是人生的一个瞬间，而人生里那些大段的时间，你会和那个懂你，爱你，珍惜你的人一起度过，好好珍惜在你身边默默陪着你的人。每个人都懂得这个道理，可是还是有人执迷不悟，苦苦执着于那个无缘的人，不肯放手，伤了自己，苦了别人。爱他或她就让那份美好的记忆藏在心底，我们都是在爱的挫折中，逐渐成长起来，才知道什么是爱，又该怎样去爱。希望多年以后再次相遇，还能洒脱的说一句，谢谢你曾经爱过我。当你的婚姻出现问题的时候，多些宽容，多些理解，少些责骂。婚姻本来就是两个人的事，一味的埋怨对方，根本不能解决任何问题。多想想曾经的美好回忆，想想两人同吃一碗面的艰难日子，想想曾经心里的感动。试着做一下换位思考，自己错在哪里，多给自己，也给对方一些机会，人都会有缺点，错误，多些包容，你会活得更轻松。如果真的缘尽了，不妨轻轻的放开手，让彼此都留下美好的回忆，道一声珍重再见，如果不能做朋友，也不可能做敌人，就做那个熟悉的陌生人吧。毕竟生活还是要继续，我们还要继续寻找那个有缘之人。当你和孩子出现问题的时候，不妨想想自己的青春年少，想想那些无知的轻狂岁月，我们都是从年轻走过来的。试着去了解今天的孩子想的什么，喜欢什么，当你以一个朋友的身份去了解他们的时候，你就已经是他们的朋友了，多些沟通，多些理解，多些包容，他们才会向你倾诉他们的烦恼，困惑，迷茫。人生不是十全十美，当你的心觉得累了的时候，不妨为自己的心找一个出口。换一个工作，换一种生活，也换一种对待生活的态度。'
+      this.articleArr = xindechukou.split('')
+      this.articleArr.forEach(v => {
+        this.article += '<span class="type-n">' + v + '</span>'
+      })
     },
     changeQqGroup () {
       alert('换群')
@@ -145,23 +171,41 @@ export default {
     help () {
       alert('帮助')
     },
-    startStroke () {
+    stroking (event) {
       this.isStroking = true
-      // 初始化跟打，需要整个初始化数组
-      if (this.currentSegment === 0) {
-        this.currentSegment = this.currentSegment + 1
-        this.articleArr = this.article.split('') // 整篇文章转成数组
-        let spansContact = ''
-        let currentTypeArr = this.articleArr.splice(0, this.currentSegment * this.defaultSplitSegmentSum)
-        for (var i in currentTypeArr) {
-          spansContact += '<span class="type-n">' + currentTypeArr[i] + '</span>'
+      // 除了需要判断开始跟打外，还要判断是不是输入了特殊的字符
+      this.keyInputSum += 1 // 这里稍候记录
+      console.log('正在输入中')
+      // TODO 如果是删除键输入，现在输入的字坐标到退格键输入的坐标区间中，修改所有class为type-n
+      // 如果是退格键
+      //    记录输入前的坐标为获取当前输入字的坐标+1 的class值为type-n
+      if (event.keyCode === 8) {
+        // let closer = this.inputText.length
+        // TODO
+        $('#article-block').children()
+      }
+    },
+    checkTrue () {
+      console.log(this.inputText)
+      // $($('#article-block .type-next')[0]).text() // 输出当前第一人未输入的字
+      console.log(this.articleArr)
+      let inputArr = this.inputText.split('')
+      for (let i = 0; i < this.articleArr.length; i++) {
+        if (i >= inputArr.length) {
+          return
         }
-        console.log(spansContact)
-        this.textSpans = spansContact
+        if (this.articleArr[i] !== inputArr[i]) {
+          console.log('错误的字是：' + this.articleArr[i] + ' 你的输入是：' + inputArr[i])
+          $($('#article-block').children()[i]).html('<span class="type-err">' + this.articleArr[i] + '</span>')
+        } else {
+          $($('#article-block').children()[i]).html('<span class="type-suc">' + this.articleArr[i] + '</span>')
+        }
       }
     },
     restroke () {
       console.log('重打')
+      // 如果this.article有值，就遍历把所有class不为type-n的改过来
+      // 无值就弹窗提示值不存在
     }
   },
   destroyed () {
@@ -260,11 +304,6 @@ export default {
   resize: none;
 }
 
-/* #control-area span {
-  display: inline;
-  float: left;
-} */
-
 .type-err {
   color: white;
   background-color: red;
@@ -273,12 +312,6 @@ export default {
 .type-suc {
   color: green;
   background-color: darkgrey;
-}
-
-.type-n {
-  border-bottom-color: red;
-  border-bottom: solid 3px;
-  border-color: red;
 }
 
 #follow-stroke-area {
